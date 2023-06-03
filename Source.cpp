c8d0ed6b730b2ad9ca0dfca00120bbfe8869e25a
LewNDZ <a_fdtnkv_a@internet.ru>
2023-06-03 10:53:44 +0300

    Unittests

---
+++ Source.cpp
@@ -0,0 +1,12 @@
+#include "tests.h"
+#include <stdexcept>
+using namespace std;
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+
+TEST_CASE("Simple test") {
+    CHECK(factorial(5) == 120);
+    CHECK_THROWS_AS(f(-1), invalid_argument);
+    CHECK(factorial(3) == 6);
+    CHECK(factorial(9) == 362880)
+}
