d329c0d94cce6d012b4152a13d6e8bbe32af0c88
S-Trateg228 <84473124+S-Trateg228@users.noreply.github.com>
2023-06-03 11:07:35 +0300

    Update func.cpp

--- func.cpp
+++ func.cpp
@@ -8,13 +8,3 @@ long long factorial(int x){
         res *= x--;
     return res;
 }
-
-int main(){
-
-    int x = 0;
-    std::cout<<"Factorial calculation\nEnter number: ";
-    std::cin>>x;
-    std::cout<<"The factorial of "<<x<<" is "<<factorial(x)<<std::endl;
-    
-    return 0;
-}
