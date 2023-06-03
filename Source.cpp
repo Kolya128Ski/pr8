055206aed9a1de58e2c3c21337d43c39ba68341b
S-Trateg228 <svyatoslavredzkin@gmail.com>
2023-06-03 11:05:22 +0300

    Tests

--- Source.cpp
+++ Source.cpp
@@ -1,11 +1,11 @@
-#include <stdexcept>
-using namespace std;
+#include <iostream>
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
+#include "func.hpp"
 
 TEST_CASE("Simple test") {
     CHECK(factorial(5) == 120);
-    CHECK_THROWS_AS(f(-1), invalid_argument);
+    CHECK_THROWS_AS(factorial(-1), std::invalid_argument);
     CHECK(factorial(3) == 6);
-    CHECK(factorial(9) == 362880)
+    CHECK(factorial(9) == 362880);
 }
